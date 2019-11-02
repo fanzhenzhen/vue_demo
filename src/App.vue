f294634ef095a86139cce95118736d69b3084b85
fanzhen <fanz@cpwxw.net>
2019-11-02 16:57:41 +0800

    commit-ui

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <Header/>
+      <List/>
+      <Footer/>
+    </div>
   </div>
 </template>
-
 <script>
+import Header from './components/Header'
+import List from './components/List'
+import Footer from './components/Footer'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    Header,
+    List,
+    Footer
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .todo-container {
+  width: 600px;
+  margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+
 </style>
